aa3a6c9bebc005f525cc73a82f3fb8b88729a81f
parinitatya <parinita20014@gmail.com>
2023-03-14 21:43:39 -0700

    finished

--- vis.js
+++ vis.js
@@ -1,77 +1,106 @@
-// Define the size and margins for the visualization
-const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+// Set up the chart dimensions
+const margin = { top: 20, right: 20, bottom: 50, left: 50 };
 const width = 600 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const height = 400 - margin.top - margin.bottom;
 
-// Append an SVG element to the body of the page
-const svg = d3.select("body")
+// Create the SVG element
+const svg = d3.select("#chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Define the scales for the visualization
-const barScale = d3.scaleRadial()
-    .range([0, height / 2])
-    .domain([0, 100]); // assuming temperatures are in Fahrenheit and range from 0 to 100
+// Load the data
+d3.csv("weatherdata.csv", (d) => ({
+    month: d.month,
+    HOU: +d.HOU,
+    NYC: +d.NYC,
+    SEA: +d.SEA,
+    MDW: +d.MDW,
+    PHL: +d.PHL,
+    PHX: +d.PHX,
+})).then((data) => {
 
-const colorScale = d3.scaleLinear()
-    .range(["#fee5d9", "#fcae91", "#fb6a4a", "#de2d26", "#a50f15"]) // example color scheme
-    .domain([0, 40, 60, 80, 100]); // example temperature breakpoints
+    // Define the x and y scales
+    const xScale = d3.scaleBand()
+        .domain(data.map((d) => d.month))
+        .range([0, width])
+        .padding(0.2);
 
-// Load the data for each city
-// d3.csv("Weather Data/CLT.csv", function (data1) {
-//     d3.csv("CQT.csv", function (data2) {
-//         d3.csv("IND.csv", function (data3) {
-//             d3.csv("JAX.csv", function (data4) {
-//                 d3.csv("KHOU.csv", function (data5) {
-//                     d3.csv("KNYC.csv", function (data6) {
-//                         d3.csv("KSEA.csv", function (data7) {
-//                             d3.csv("MDW.csv", function (data8) {
-//                                 d3.csv("PHL.csv", function (data9) {
-//                                     d3.csv("PHX.csv", function (data10) {
-//                                         // Merge the data into a single array
-//                                         const data = data1.concat(data2, data3, data4, data5, data6, data7, data8, data9, data10);
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => d.HOU)])
+        .range([height, 0]);
 
-//                                         // Your code for creating the visualization goes here...
-//                                     });
-//                                 });
-//                             });
-//                         });
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
+    // Create the x and y axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("transform", function () {
+            return "translate(0, " + height + ")"
+        })
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
+    // Add x-axis title
+    svg.append("text")
+        .attr("class", "x-axis-title")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + margin.top + 20)
+        .text("Month");
+
+    // Add y-axis title
+    svg.append("text")
+        .attr("class", "y-axis-title")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .text("Temperature (°F)");
 
 
-Promise.all([d3.csv("../WeatherData/CLT.csv"),
-d3.csv("../WeatherData/CQT.csv"),
-d3.csv("../WeatherData/IND.csv"),
-d3.csv("../WeatherData/JAX.csv"),
-d3.csv("../WeatherData/KHOU.csv"),
-d3.csv("../WeatherData/KNYC.csv"),
-d3.csv("../WeatherData/KSEA.csv"),
-d3.csv("../WeatherData/MDW.csv"),
-d3.csv("../WeatherData/PHL.csv"), 
-d3.csv("../WeatherData/PHX.csv")]).then(function(files) {   
-    
-// set up the svg container  var width = 960,  height = 600;  var svg = d3...
+    // Define a color scale
+    const colorScale = d3.scaleSequential()
+        .domain([d3.min(data, d => d.HOU), d3.max(data, d => d.HOU)])
+        .interpolator(d3.interpolateReds);
 
-// Compute the average record high temperature for each city for each month
-const cityMonthAverages = [];
-cities.forEach(city => {
-    const cityData = data.filter(d => d.City === city);
-    const months = d3.map(cityData, d => d.Month).keys();
-    months.forEach(month => {
-        const monthData = cityData.filter(d => d.Month === month);
-        const averageTemperature = d3.mean(monthData, d => d.RecordHigh);
-        cityMonthAverages.push({ city, month, averageTemperature });
+
+    // Create the bars
+    let bars = svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => xScale(d.month))
+        .attr("width", xScale.bandwidth())
+        .attr("y", (d) => yScale(d.HOU))
+        .attr("height", (d) => height - yScale(d.HOU))
+        .attr("fill", (d) => colorScale(d.HOU));
+
+    // Add a class to the dropdown for styling purposes
+    d3.select("#cities")
+        .classed("dropdown", true);
+
+    // Add interactivity to the chart
+    d3.select("#cities").on("change", () => {
+        const selectedCity = d3.select("#cities").node().value;
+
+        // Update the y scale based on the selected city
+        yScale.domain([0, d3.max(data, (d) => d[selectedCity])]);
+
+        // Update the color scale based on the selected city
+        colorScale.domain([d3.min(data, d => d[selectedCity]), d3.max(data, d => d[selectedCity])]);
+
+        // Update the bars' positions, heights, and colors based on the selected city
+        bars.data(data)
+            .transition()
+            .duration(500)
+            .attr("y", (d) => yScale(d[selectedCity]))
+            .attr("height", (d) => height - yScale(d[selectedCity]))
+            .attr("fill", (d) => colorScale(d[selectedCity]));
     });
-});
-
-// 
-
-
 });
